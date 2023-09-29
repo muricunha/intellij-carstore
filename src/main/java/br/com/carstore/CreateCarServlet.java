36d89c339f2acd57b9b9661bb24f5fe4542a757f
muricunha <muriloscunha23@gmail.com>
2023-09-28 21:17:56 -0300

    subindo lab 2

---
+++ src/main/java/br/com/carstore/CreateCarServlet.java
@@ -0,0 +1,31 @@
+package br.com.carstore;
+
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String carName = request.getParameter("car-name");
+
+        Car car = new Car(carName);
+
+        CarDao carDao = new CarDao();
+
+        carDao.createCar(car);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+
+    }
+
+}
